b3f7e2d7278868710485cf8c1f9ca2202fc49a79
Chouquette <lydie.sts@gmail.com>
2024-09-04 16:26:41 -0400

    Exercice 2 : étape 5
    mise en page et opacité de l'image

--- HappyBirthday/app/src/main/java/com/progmobile/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/progmobile/happybirthday/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,7 +28,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HappyBirthdayTheme {
-               GreetingText(message = "Happy Birthday Lydie!",
+               GreetingImage(message = "Happy Birthday Lydie!",
                             from = "From Némo",
                             modifier = Modifier
                                 .padding(8.dp)
@@ -67,11 +69,20 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
 fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier)
 {
     val image = painterResource(id = R.drawable.cayenne)
-    Image(
-        painter = image,
-        contentDescription = "Photo de Queen Cayenne, une chatte grise très mignonne avec le" +
-                "ventre blanc. Elle est en train de faire sa toilette sur le canapé rouge."
-    )
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = "Photo de Queen Cayenne, une chatte grise très mignonne avec le" +
+                    "ventre blanc. Elle est en train de faire sa toilette sur le canapé rouge.",
+            contentScale = ContentScale.Crop,
+            alpha = 0.7F       )
+        GreetingText(message = "Happy Birthday Lydie!",
+            from = "From Némo",
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxSize()
+        )
+    }
 }
 
 
