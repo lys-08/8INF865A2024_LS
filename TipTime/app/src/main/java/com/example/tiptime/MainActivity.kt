c9017e2e3cb64e204adb5bdab70e91a1d3a638ca
Lydie Santos <lydie.sts@gmail.com>
2024-09-10 17:28:56 -0400

    Exo 2 : étape 5
    Ajout de boutons d'actions

--- TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -43,6 +43,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -93,6 +94,10 @@ fun TipTimeLayout() {
         )
         EditNumberField(
             label = R.string.bill_amount,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
             value = amountInput,
             onValueChanged = {amountInput = it},
             modifier = Modifier .padding(bottom = 32.dp)
@@ -100,6 +105,10 @@ fun TipTimeLayout() {
         )
         EditNumberField(
             label = R.string.how_was_the_service,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
             value = tipInput,
             onValueChanged = {tipInput = it},
             modifier = Modifier .padding(bottom = 32.dp)
@@ -114,13 +123,17 @@ fun TipTimeLayout() {
 }
 
 @Composable
-fun EditNumberField(@StringRes label : Int, value : String, onValueChanged : (String) -> Unit, modifier: Modifier = Modifier){
+fun EditNumberField(@StringRes label : Int,
+                    keyboardOptions: KeyboardOptions,
+                    value : String,
+                    onValueChanged : (String) -> Unit,
+                    modifier: Modifier = Modifier){
     TextField(
         value = value,
         onValueChange = onValueChanged,
         label = { Text(stringResource(label)) },
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        keyboardOptions = keyboardOptions,
         modifier = modifier
     )
 }
