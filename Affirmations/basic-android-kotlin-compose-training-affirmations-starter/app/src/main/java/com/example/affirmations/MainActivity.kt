f0de83a3d5c9c2d968531cb375c967cec2f376d2
Lydie Santos <lydie.sts@gmail.com>
2024-09-22 16:20:21 -0400

    Affichage de la liste

--- Affirmations/basic-android-kotlin-compose-training-affirmations-starter/app/src/main/java/com/example/affirmations/MainActivity.kt
+++ Affirmations/basic-android-kotlin-compose-training-affirmations-starter/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -20,10 +20,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -32,10 +40,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.affirmations.data.Datasource
 import com.example.affirmations.model.Affirmation
 import com.example.affirmations.ui.theme.AffirmationsTheme
 
@@ -59,6 +69,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AffirmationsApp() {
+    val layoutDirection = LocalLayoutDirection.current
+
+    Surface (modifier = Modifier
+        .fillMaxSize()
+        .statusBarsPadding()
+        .padding(
+            start = WindowInsets.safeDrawing
+                .asPaddingValues()
+                .calculateStartPadding(layoutDirection),
+            end = WindowInsets.safeDrawing
+                .asPaddingValues()
+                .calculateEndPadding(layoutDirection)
+        )
+            )
+    {
+        AffirmationList(affirmationList = Datasource().loadAffirmations())
+    }
 }
 
 @Composable
@@ -79,6 +106,17 @@ fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier)
                 modifier = Modifier.padding(16.dp),
                 style = MaterialTheme.typography.headlineSmall
             )
+        }
+    }
+}
+
+@Composable
+fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier)
+{
+    LazyColumn(modifier = modifier) {
+        items(affirmationList) { affirmation ->
+            AffirmationCard(affirmation = affirmation,
+                modifier = Modifier.padding(8.dp))
         }
     }
 }
