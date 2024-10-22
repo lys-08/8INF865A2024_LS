51f469c429c19db7ce7782ac48a6881ea899cd47
Lydie Santos <lydie.sts@gmail.com>
2024-10-22 15:03:21 -0400

    étape 5 : ajout d'un viewModel

---
+++ Unscramble/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.unscramble.ui
+
+import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.allWords
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class GameViewModel : ViewModel() {
+    // Game UI State
+    private val _uiState = MutableStateFlow(GameUiState())
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf() // Set of words used in the game
+
+    private fun shuffleCurrentWord(word: String): String {
+        val tmpWord = word.toCharArray()
+
+        // Scramble the word
+        tmpWord.shuffle()
+        while (String(tmpWord).equals(word)) {
+            tmpWord.shuffle()
+        }
+        return String(tmpWord)
+    }
+
+    private fun pickRandomWordAndShuffle(): String {
+        // Picking a word that hasn't been used before
+        currentWord = allWords.random()
+
+        if (usedWords.contains(currentWord)) {
+            return pickRandomWordAndShuffle()
+        } else {
+            usedWords.add(currentWord)
+            return shuffleCurrentWord(currentWord)
+        }
+    }
+
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+    }
+
+    init {
+        resetGame()
+    }
+}
